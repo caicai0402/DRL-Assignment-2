d5d699e78ae784b110f652840ec621e54a5d68bf
caicai0402 <caicaihu0402@gmail.com>
2025-04-04 18:32:18 +0800

    update student_agent.py for test

--- student_agent.py
+++ student_agent.py
@@ -3,7 +3,9 @@ from Game2048Env import Game2048Env
 import random
 
 def get_action(state, score):
-    # env = Game2048Env()
+    env = Game2048Env()
+
+    print(env.score)
     return random.choice([0, 1, 2, 3]) # Choose a random action
     
     # You can submit this random agent to evaluate the performance of a purely random strategy.
