ebed1e20768a6d496a916dc62e033e6713e790e3
caicai0402 <caicaihu0402@gmail.com>
2025-04-05 16:02:30 +0800

    add round and score info when simulating

--- play2048.py
+++ play2048.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     state = env.reset()
     # env.render()
 
-    done = False
+    round_no, done = 0, False
     while not done:
-        action = get_action(env.board, env.score)
+        action = get_action(env.board.copy(), env.score)
         state, reward, done, _ = env.step(action)
+        round_no += 1
+        print("Round No.:", round_no, "Current score:", env.score)
         # env.render(action=action)  # Display the updated game state
 
     print("Game over, final score:", env.score)
-    
